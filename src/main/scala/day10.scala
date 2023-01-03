16b3b4e5ffe6108403ce743287ccff19fea06f80
Daniel Strebel <daniel.strebel@gmail.com>
2023-01-03 08:51:13 +0100

    day10

--- src/main/scala/day10.scala
+++ src/main/scala/day10.scala
@@ -1,4 +1,22 @@
+import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
+
+object crt {
+  private val pixels: Array[Boolean] = new Array(40*6)
+  def setPixel(cycle: Int, sprite: Int): Unit = {
+    val p = (cycle-1)
+    val col = p%40
+    if (Math.abs(sprite - col) <= 1) {
+      pixels(p)=true
+    } else {
+      pixels(p)=false
+    }
+  }
+
+  override def toString: String = {
+    pixels.grouped(40).toArray.map(row => row.map(p => if (p) "#" else '.' ).mkString + "\n").mkString
+  }
+}
 
 object day10 extends App {
   private val instructions = Source.fromURI(getClass.getResource("day10.txt").toURI)
@@ -6,7 +24,6 @@ object day10 extends App {
 
   def handleInterestingSignal(cycle: Int, register: Int): Int = {
     if ((cycle - 20) % 40 == 0) {
-      println(cycle, register)
       cycle*register
     } else {
       0
@@ -21,13 +38,16 @@ object day10 extends App {
      instr match {
        case "noop" => {
          sumInterestingSignals += handleInterestingSignal(cycle, register)
+         crt.setPixel(cycle, register)
          cycle += 1
        }
        case s if s.startsWith("addx") => {
          sumInterestingSignals+=handleInterestingSignal(cycle, register)
+         crt.setPixel(cycle, register)
          cycle+=1
          val opValue = s.split(" ")(1).toInt
          sumInterestingSignals+=handleInterestingSignal(cycle, register)
+         crt.setPixel(cycle, register)
          register+=opValue
          cycle+=1
        }
@@ -35,4 +55,7 @@ object day10 extends App {
    })
 
   println(s"Day1 Part1: ${sumInterestingSignals}")
+
+  println("Day10 Part2")
+  print(crt)
 }
